9ed157a8df352c41148afe7d2271aca7aa03613a
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2023-01-30 23:24:01 -0700

    Improve codebase (#1)
    * Update tox.ini
    
    Create tox.ini
    
    Create lint.yml
    
    * Update tox.ini
    
    * Create play_data.py
    
    * Create get_boss_hp.py
    
    * Create run_model.py
    
    * Create train_model.py
    
    * Create collect_data.py
    
    * Delete dataCollector.py
    
    * Delete get_hp.py
    
    * Delete play_data.py
    
    * Delete run_model.py
    
    * Delete train_model.py
    
    * Update lint.yml
    
    * Update run_model.py
    
    * Update train_model.py
    
    * Update tox.ini
    
    * Update train_model.py

---
+++ elden_bot/run_model.py
@@ -0,0 +1,122 @@
+import tensorflow as tf
+import time
+import cv2
+import mss
+import numpy as np
+import os
+from collections import deque
+import pydirectinput
+
+# Better performance
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+def press_keys(p):
+    vals = ["w", "a", "s", "d", "f", "r", "u", "i", "o", "p", " "]
+    for i in range(len(p[0])):
+        if p[0][i] > 0.5:
+            pydirectinput.keyDown(vals[i], _pause=False)
+            print(vals[i])
+        else:
+            pydirectinput.keyUp(vals[i], _pause=False)
+
+
+def end():
+    print("Done")
+    ending = time.time()
+    duration = ending - init
+    print(f"Average FPS: {count/duration}")
+
+
+def keys_to_array(keys):
+    # [W, A, S, D, F, R, U, I, O, P, SPACE]
+    x = np.zeros(shape=(11), dtype=np.uint8)
+    for key in keys:
+        if key in indexes:
+            x[indexes[key]] = 1
+    return x
+
+
+if __name__ == "__main__":
+    init = tf.keras.initializers.VarianceScaling(scale=2)
+    inputs = tf.keras.layers.Input(shape=(244, 138, 3))
+    num_actions = 11
+    x = tf.keras.layers.Conv2D(
+        32, 8, strides=(4, 4), activation="relu", kernel_initializer=init
+    )(inputs)
+    x = tf.keras.layers.Conv2D(
+        64, 4, strides=(3, 3), activation="relu", kernel_initializer=init
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        64, 3, strides=(1, 1), activation="relu", kernel_initializer=init
+    )(x)
+    # x = tf.keras.layers.Conv2D(128, 3, strides=(1,1), activation='relu')(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    # x = tf.keras.layers.Dense(1024, activation='relu', kernel_initializer=init)(x)
+    # x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dense(512, activation="relu", kernel_initializer=init)(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dense(num_actions, activation="sigmoid")(x)
+    model = tf.keras.models.Model(inputs=inputs, outputs=x)
+    # checkpoint_filepath = './small_classweight/checkpoint'
+    checkpoint_filepath = "./video_test/checkpoint"
+    model.load_weights(checkpoint_filepath)
+
+    model(np.random.uniform(-1, 1, (1, 244, 138, 3)))
+
+    init_count = 0
+    width = 244
+    # ratio =  width / 2048
+    new_height = 138  # int(1152 * ratio)
+    left = 908
+    down = 495
+    width2 = 8
+    height = 1067
+
+    max_health = None
+
+    for i in range(5):
+        print(i)
+        time.sleep(1)
+    init = time.time()
+    inputs = deque(maxlen=3)
+    with mss.mss() as sct:
+        try:
+            count = 0
+            while True:
+                # Part of the screen to capture
+                monitor = {"top": 50, "left": 0, "width": 2048, "height": 1152}
+
+                # Get raw pixels from the screen, save it to a Numpy array
+                img = np.array(sct.grab(monitor))
+
+                img2 = img[left : left + width, down : down + height]
+                ret, threshb = cv2.threshold(img2[:, :, 0], 190, 255, cv2.THRESH_BINARY)
+                ret, threshg = cv2.threshold(img2[:, :, 1], 190, 255, cv2.THRESH_BINARY)
+                ret, threshr = cv2.threshold(img2[:, :, 2], 190, 255, cv2.THRESH_BINARY)
+                summed = threshb + threshg + threshr
+                value = np.max(np.argmax(summed, axis=1))
+                if count == 0:
+                    max_health = value
+                if max_health < 1:
+                    max_health = 1000
+
+                value = -value / max_health
+                img = cv2.resize(img, (width, new_height), interpolation=cv2.INTER_AREA)
+                img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+                if count == 0:
+                    for _ in range(3):
+                        inputs.append(img)
+                else:
+                    inputs.append(img)
+                # (old, mid, newest)
+                img = np.array(inputs)
+                img = np.expand_dims(img, axis=0).transpose(0, 3, 2, 1)
+
+                pred = model(img)
+                press_keys(pred)
+                count += 1
+
+        except KeyboardInterrupt:
+            end()
--- elden_bot/train_model.py
+++ elden_bot/train_model.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
-#from tensorflow.keras.applications.efficientnet import EfficientNetB2
-#from tensorflow.keras.applications.resnet50 import ResNet50
-#from tensorflow.keras.applications.inception_v3 import InceptionV3
-import time 
+
+# from tensorflow.keras.applications.efficientnet import EfficientNetB2
+# from tensorflow.keras.applications.resnet50 import ResNet50
+# from tensorflow.keras.applications.inception_v3 import InceptionV3
 import numpy as np
 import wandb
 from wandb.keras import WandbCallback
-import os 
 
-#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+# import os
+# os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 actions = ["W", "A", "S", "D", "F", "R", "U", "I", "O", "P", " "]
 num_actions = len(actions)
@@ -16,31 +16,31 @@ num_actions = len(actions)
 num_runs = 32
 folderStrings = ["test_images"]
 for i in range(1, num_runs + 1):
-	folderStrings.append(f'test_images{i}')
+    folderStrings.append(f"test_images{i}")
 
 data = []
 labels = []
 
 for folderString in folderStrings:
-	batch = 100
-	init = 0
-	end = batch
-	i = 0
-	while True:
-		try:
-			if i == init:
-				ins = np.load(f'{folderString}/{init}_{end}.npy')
-				images = np.load(f'{folderString}/img{init}_{end}.npy')
-			if i == end:
-				init += 100
-				end += 100
-				ins = np.load(f'{folderString}/{init}_{end}.npy')
-				images = np.load(f'{folderString}/img{init}_{end}.npy')
-			data.append(images)
-			labels.append(ins)
-			i += batch
-		except FileNotFoundError:
-			break
+    batch = 100
+    init = 0
+    end = batch
+    i = 0
+    while True:
+        try:
+            if i == init:
+                ins = np.load(f"{folderString}/{init}_{end}.npy")
+                images = np.load(f"{folderString}/img{init}_{end}.npy")
+            if i == end:
+                init += 100
+                end += 100
+                ins = np.load(f"{folderString}/{init}_{end}.npy")
+                images = np.load(f"{folderString}/img{init}_{end}.npy")
+            data.append(images)
+            labels.append(ins)
+            i += batch
+        except FileNotFoundError:
+            break
 
 data = np.vstack(data)
 labels = np.vstack(labels)
@@ -65,7 +65,7 @@ print(data.shape)
 params = {}
 params["learning_rate"] = 3e-4
 params["epochs"] = 1000
-params["split"] = 9/10
+params["split"] = 9 / 10
 params["batch_size"] = 128
 params["shuffle"] = 100 * num_runs
 params["base_model"] = "custom"
@@ -85,38 +85,44 @@ train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
 test_dataset = test_dataset.batch(BATCH_SIZE)
 
 inputs = tf.keras.layers.Input(shape=(244, 138, 3))
-'''
+"""
 if params["base_model"] == "InceptionV3":
 	base_model = InceptionV3(weights=None, include_top=False, pooling='avg', input_tensor=inputs)
 x = base_model.output
 x = tf.keras.layers.Dense(num_actions, activation='sigmoid')(x)
 model = tf.keras.models.Model(inputs=inputs, outputs=x)
-'''
+"""
 
 init = tf.keras.initializers.VarianceScaling(scale=2)
-x = tf.keras.layers.Conv2D(32, 8, strides=(4,4), activation='relu', kernel_initializer=init)(inputs)
-x = tf.keras.layers.Conv2D(64, 4, strides=(3,3), activation='relu', kernel_initializer=init)(x)
-x = tf.keras.layers.Conv2D(64, 3, strides=(1,1), activation='relu', kernel_initializer=init)(x)
-#x = tf.keras.layers.Conv2D(128, 3, strides=(1,1), activation='relu')(x)
+x = tf.keras.layers.Conv2D(
+    32, 8, strides=(4, 4), activation="relu", kernel_initializer=init
+)(inputs)
+x = tf.keras.layers.Conv2D(
+    64, 4, strides=(3, 3), activation="relu", kernel_initializer=init
+)(x)
+x = tf.keras.layers.Conv2D(
+    64, 3, strides=(1, 1), activation="relu", kernel_initializer=init
+)(x)
+# x = tf.keras.layers.Conv2D(128, 3, strides=(1,1), activation='relu')(x)
 x = tf.keras.layers.Flatten()(x)
 x = tf.keras.layers.Dropout(0.3)(x)
-#x = tf.keras.layers.Dense(1024, activation='relu', kernel_initializer=init)(x)
-#x = tf.keras.layers.Dropout(0.3)(x)
-x = tf.keras.layers.Dense(512, activation='relu', kernel_initializer=init)(x)
+# x = tf.keras.layers.Dense(1024, activation='relu', kernel_initializer=init)(x)
+# x = tf.keras.layers.Dropout(0.3)(x)
+x = tf.keras.layers.Dense(512, activation="relu", kernel_initializer=init)(x)
 x = tf.keras.layers.Dropout(0.3)(x)
-x = tf.keras.layers.Dense(num_actions, activation='sigmoid')(x)
+x = tf.keras.layers.Dense(num_actions, activation="sigmoid")(x)
 model = tf.keras.models.Model(inputs=inputs, outputs=x)
 model.summary()
 
 opt = tf.keras.optimizers.Adam(learning_rate=params["learning_rate"])
 loss = tf.keras.losses.BinaryCrossentropy()
-metrics = ['accuracy']
+metrics = ["accuracy"]
 
 model.compile(optimizer=opt, loss=loss, metrics=metrics)
 
 wandb.init(project="supervised_runs", entity="elden_ring_ai", name="video_example1")
 
-checkpoint_filepath = './video_test/checkpoint'
+checkpoint_filepath = "./video_test/checkpoint"
 callbacks = [
     tf.keras.callbacks.EarlyStopping(
         # Stop training when `val_loss` is no longer improving
@@ -139,12 +145,20 @@ callbacks = [
         monitor="val_loss",
         verbose=1,
     ),
-    WandbCallback()
+    WandbCallback(),
 ]
 
-#actions = ["W", "A", "S", "D", "F", "R", "U", "I", "O", "P", " "]
-weights = [1/(np.sum(labels[:,i]) + 1) * labels.shape[0] for i in range(len(labels[0]))]
+# actions = ["W", "A", "S", "D", "F", "R", "U", "I", "O", "P", " "]
+weights = [
+    1 / (np.sum(labels[:, i]) + 1) * labels.shape[0] for i in range(len(labels[0]))
+]
 print(weights)
 class_weight = {i: weights[i] for i in range(len(weights))}
 
-model.fit(train_dataset, epochs=params["epochs"], validation_data=test_dataset, callbacks=callbacks, class_weight=class_weight)
+model.fit(
+    train_dataset,
+    epochs=params["epochs"],
+    validation_data=test_dataset,
+    callbacks=callbacks,
+    class_weight=class_weight,
+)
